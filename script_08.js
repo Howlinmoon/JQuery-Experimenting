0eeb81c9c6baa65460c63c97b66f0a35f4d3f676
Jim Veneskey <github@strifecrafter.com>
2015-01-25 11:45:07 -0500

    A very brief mention that Ajax can use call back functions named 'success'

---
+++ script_08.js
@@ -0,0 +1,18 @@
+
+// success is called when the ajax call is done
+
+$.ajax({ 
+	url: '/path',
+	data: {id: '123', 'name': 'Jim'},
+	success: function() {
+		
+		
+	}
+});
+
+//name: 'Jim',
+//school: 'CSU',
+//callback: function(x) {
+//	alert(x);
+//},
+//location: 'Barberton'
